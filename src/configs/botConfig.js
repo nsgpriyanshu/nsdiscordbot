eb7a22af48b2489d296f0eb604a0227fd78313c5
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-25 17:53:13 +0530

    feat: add config and const files
    Added the modified config and const files for this discord app

---
+++ src/configs/botConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    BOT_TOKEN: 'your-bot-token',
+    BOT_ID: 'your-bot-id',
+    DEVELOPER_IDS: ['your-developer-id'],
+    ERROR_CHANNEL: 'your-error-channel-id',
+    PREFIX: '!',
+  };
+  
---
+++ src/constants/botConst.js
@@ -0,0 +1,11 @@
+module.exports = {
+    COLORS: {
+      red: '#ff3b30',
+      yellow: '#ffcc00',
+    },
+    EMOJIS: {
+      failed: '❌',
+      caution: '⚠️',
+    },
+  };
+  
