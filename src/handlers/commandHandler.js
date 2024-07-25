a89c54a6ce66862bdb5d1d71404e67508599edb5
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-25 18:00:54 +0530

    feat: add handlers to run the bot smooothly

---
+++ src/handlers/commandHandler.js
@@ -0,0 +1,56 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const { readdirSync, statSync } = require('fs');
+const { join, extname } = require('path');
+const { logger } = require('../utils/logger');
+const config = require('../configs/botConfig');
+
+const loadSlashCommands = (dirs) => {
+  return new Promise((resolve, reject) => {
+    const commands = [];
+
+    const loadCommandsFromDir = async (dir) => {
+      const files = readdirSync(dir);
+
+      for (const file of files) {
+        const filePath = join(dir, file);
+        const fileStat = statSync(filePath);
+
+        if (fileStat.isDirectory()) {
+          await loadCommandsFromDir(filePath);
+        } else if (extname(file) === '.ts' || extname(file) === '.js') {
+          const commandModule = await import(filePath);
+          const command = commandModule.default;
+          if (command && command.data instanceof SlashCommandBuilder) {
+            commands.push(command.data.toJSON());
+          }
+        }
+      }
+    };
+
+    Promise.all(dirs.map(dir => loadCommandsFromDir(dir)))
+      .then(() => resolve(commands))
+      .catch(reject);
+  });
+};
+
+const absoluteIncludeDirectories = ['commands/slashCommands/general', 'commands/slashCommands/info'].map(dir => join(__dirname, '../', dir));
+
+loadSlashCommands(absoluteIncludeDirectories)
+  .then(commands => {
+    const rest = new REST({ version: '10' }).setToken(config.BOT_TOKEN);
+    logger.log('Started refreshing application (/) commands.');
+
+    return rest.put(Routes.applicationCommands(config.BOT_ID), { body: commands });
+  })
+  .then(() => {
+    logger.log('Successfully reloaded application (/) commands.');
+  })
+  .catch(error => {
+    if (error instanceof Error) {
+      logger.error(`Error registering commands: ${error.message}`);
+    } else {
+      logger.error('Unknown error occurred while registering commands.');
+    }
+  });
+
+module.exports = { loadSlashCommands };
