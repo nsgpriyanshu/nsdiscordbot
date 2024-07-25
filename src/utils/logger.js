f2a6f22c63ec63454302a530ff7d52448de2fabc
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-25 17:54:54 +0530

    feat: add logger utilitie for better logging

---
+++ src/utils/logger.js
@@ -0,0 +1,10 @@
+const { logPastelGreen, logPastelLavender, logPastelRed, logPastelYellow } = require('nstypocolors');
+
+const logger = {
+  log: (message) => logPastelGreen(`[LOG] ${message}`),
+  sLog: (message) => logPastelLavender(`[SUCCESS] ${message}`),
+  warn: (message) => logPastelYellow(`[WARN] ${message}`),
+  error: (message) => logPastelRed(`[ERROR] ${message}`),
+};
+
+module.exports = { logger };
